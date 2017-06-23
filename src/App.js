99b2226e6e3b0a2e55eb53f22a689e0eb4c5b30d
James Byrne <mail@jamestbyrne.com>
2017-06-23 11:24:25 +0100

    Hooked up to API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+const DEFAULT_QUERY = '';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+let url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
 const list = [
   {
     title: 'React',
@@ -26,16 +35,37 @@ function isSearched (searchTerm) {
   };
 };
 
+// Main App component
 class App extends Component {
 constructor(props) {
   super(props);
   this.state = {
-    list,
-    searchTerm : '',
+  	result: null,
+  	searchTerm: DEFAULT_QUERY,
   };
 
+  this.setSearchTopStories = this.setSearchTopStories.bind(this);
+  this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
   this.onSearchChange = this.onSearchChange.bind(this);
   this.onDismiss = this.onDismiss.bind(this);
+}
+
+setSearchTopStories(result) {
+	this.setState({result});
+}
+
+fetchSearchTopStories(searchTerm) {
+	fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+	.then(response => response.json())
+	.then((result) => {
+		this.setSearchTopStories(result)
+		console.log(result)
+	});
+}
+
+componentDidMount() {
+	const { searchTerm } = this.state;
+	this.fetchSearchTopStories(searchTerm);
 }
 
 onSearchChange(e) {
@@ -53,17 +83,18 @@ onDismiss(id) {
 
 
   render() {
-     const { searchTerm, list } = this.state;
+     const { searchTerm, result } = this.state;
+
+     if (!result) { return null; }
     return (
-      <div className="App">
+      <div className="page">
 
 	      <Search
 	      value={searchTerm}
-	      onChange={this.onSearchChange}
-	      />
+	      onChange={this.onSearchChange}/>
 
 	      <Table 
-	      list={list}
+	      list={result.hits}
 	      pattern={searchTerm}
 	      onDismiss={this.onDismiss}
 	      />
@@ -73,53 +104,66 @@ onDismiss(id) {
 	}
 }
 
-class Search extends Component {
-	render() {
-		const { value, onChange } = this.props;
-		return (
-			<form>
-				<input
-				type="text"
-				value={value}
-				onChange={onChange}
-				/>
-			</form>
-		);
-	}
+// Search component
+
+const Search = ({ value, onChange, children }) => {
+	return (
+	<header className="site-header">
+		<form>
+			{children} <input
+			type="text"
+			value={value}
+			className="search-bar"
+			onChange={onChange}
+			/>
+		</form>
+	</header>		
+	);
 }
 
 
-class Table extends Component {
-	render() {
-		const { list, pattern, onDismiss } = this.props;
-		return (
 
-			<div>
-			{ list.filter(isSearched(pattern)).map(item =>
-				<div key={item.objectID}>
+
+// Table and button component
+
+const Table = ({list, pattern, onDismiss}) => {
+	let filtered = list.filter(isSearched(pattern));
+	function compare(a, b) {
+		if (a.created_at_i < b.created_at_i)
+			return -1;
+		if (a.created_at_i > b.created_at_i)
+			return 1;
+		return 0;
+	}
+
+	console.log(filtered.sort(compare))
+	return (
+					<main className="table">
+			{ 
+				
+				filtered.map(item =>
+				<div key={item.objectID} className="table-row">
 				<span>
 					<a href={item.url}>{item.title}</a>
 				</span>
 				<span>{item.author}</span>
 				<span>{item.num_comments}</span>
 				<span>{item.points}</span>
+				<span>{item.created_at_i}</span>
 				<span>
 					<button
 						onClick={() => onDismiss(item.objectID)}
 						type="button"
+						className="button-inline"
 					>
 						Dismiss
 					</button>
 				</span>
 			</div>
-				)}
-			</div>	
-
-		);
-	}
+			)}
+			</main>
+	)
 }
-
-
 
 
 export default App;
